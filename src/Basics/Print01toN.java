904bda513613b1e2ed3fb45d137a3ffc5d2da38a
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-07-29 15:46:51 +0530

    Add Print01toN java program

---
+++ src/Basics/Print01toN.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Print01toN {
+    public static void main(String[]args){
+        int num = 0;
+        for(int i = 1; i<=5; i++){
+            System.out.println(i);
+        }
+    }
+}
