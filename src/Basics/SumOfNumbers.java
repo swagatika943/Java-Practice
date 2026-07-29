fb6527af7c0ebaf90fdff772d6e471538ee3370b
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-07-29 16:02:13 +0530

    Add SumOfNumbers java program

---
+++ src/Basics/SumOfNumbers.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class SumOfNumbers {
+    public static void main(String [] args){
+        int num = 10;
+        int sum = 0;
+        for(int i=0; i<=10; i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
