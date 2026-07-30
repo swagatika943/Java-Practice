ee333cbf89f3282b95b62371b4996074fffc6b16
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-07-30 14:47:57 +0530

    Add PrimeNum java program

--- src/Basics/PrimeNum.java
+++ src/Basics/PrimeNum.java
@@ -1,11 +1,34 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class PrimeNum {
     public static void main(String[]args){
-        int num = 6;
+//        int num = 5;
+//        int count = 0;
+//        for(int i=1; i<=num; i++){
+//            if(num%i==0){
+//                count++;
+//            }
+//        }
+//        if(count == 2){
+//            System.out.println("Prime num");
+//        } else {
+//            System.out.println("Not prime num");
+//        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no");
+        int num = sc.nextInt();
         int count = 0;
         for(int i=1; i<=num; i++){
-//            if(int )
+            if(num%i==0){
+                count++;
+            }
+        }
+        if(count == 2) {
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
         }
     }
 }
