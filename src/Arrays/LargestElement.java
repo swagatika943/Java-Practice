54f412aa20e8d453629d6de5cec6b6ceb2de4265
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-08-29 15:37:48 +0530

    Add Array programs

---
+++ src/Arrays/LargestElement.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 7, 40, 15};
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        System.out.println("Largest = " + largest);
+    }
+}
