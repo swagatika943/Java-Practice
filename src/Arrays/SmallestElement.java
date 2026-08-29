8df93e01ee8f193c498ef0e5273b37d2d7dc8983
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-08-29 15:38:53 +0530

    Add Array programs

---
+++ src/Arrays/SmallestElement.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class SmallestElement {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 7, 40, 15};
+        int smallest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        System.out.println("Smallest = " + smallest);
+    }
+}
