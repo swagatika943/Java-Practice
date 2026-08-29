e9bbcd1c588f802a8ab089cba294f28cd4cf378f
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-08-29 15:38:37 +0530

    Add Array programs

---
+++ src/Arrays/SecondSmallestElement.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class SecondSmallestElement {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 7, 40, 15};
+        int smallest = arr[0];
+        int secondSmallest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            }
+            else if (arr[i] < secondSmallest && arr[i] != smallest) {
+                secondSmallest = arr[i];
+            }
+        }
+        System.out.println("Second Smallest = " + secondSmallest);
+    }
+}
