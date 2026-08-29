186eab896f486e4804261fdb20a841d39989b3c2
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-08-29 15:38:21 +0530

    Add Array programs

---
+++ src/Arrays/SecondLargestElement.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class SecondLargestElement {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 7, 40, 15};
+        int largest = arr[0];
+        int secondLargest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        System.out.println("Second Largest = " + secondLargest);
+    }
+}
