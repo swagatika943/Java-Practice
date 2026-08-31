e62a7af67268785c2e629d1fa496918892b1336f
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-08-31 14:26:05 +0530

    Add Array programs

---
+++ src/Arrays/Level2/CopyArray.java
@@ -0,0 +1,16 @@
+package Arrays.Level2;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr1 = {10, 20, 30, 40, 50};
+        int[] arr2 = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+        System.out.print("Copied Array:");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}
