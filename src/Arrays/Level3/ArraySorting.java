bfb59c22c895b9a484e498249ccceeb76d545769
Swayan Swagatika <swayanswagatika01@gmail.com>
2026-09-16 14:20:22 +0530

    Add Array programs

---
+++ src/Arrays/Level3/ArraySorting.java
@@ -0,0 +1,20 @@
+package Arrays.Level3;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        int[] arr = {40, 10, 30, 20, 50};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
